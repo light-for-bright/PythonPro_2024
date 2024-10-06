748bc3c0e79b2dfb29d309917f58f94df9e0ef7c
light-for-bright <teacherpavel4@gmail.com>
2024-10-06 14:29:40 +0300

    lesson2

---
+++ work/lesson_1.py
@@ -0,0 +1,32 @@
+n = 30
+# print(n)
+'''print("Введите текст")
+b = input("Введите текст")'''
+
+"""if n:
+    print("Да")
+else:
+    print("Нет")"""
+
+"""if n > 20:
+    print(n)
+else:
+    print("n <= 20")"""
+
+"""if print(n):
+    print("Верно")
+else:
+    print("Неверно")"""
+
+"""if input("Ввод: "):
+    print("Верно")
+elif n > 20:
+    print(n)
+else:
+    print("Неверно")
+"""
+
+print_for_users = [1,2,3,8,9]
+for i in print_for_users:
+    print(i)
+
